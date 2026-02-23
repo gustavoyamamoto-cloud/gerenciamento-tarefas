a0e542b28f7e583019a08da99b9da0986170d08d
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-02-23 13:43:54 -0300

    Primeiro commit do projeto Spring Boot

---
+++ sitemaGerenciamentoTarefas/src/main/java/com/gustavo/sitemaGerenciamentoTarefas/SitemaGerenciamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.sitemaGerenciamentoTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaGerenciamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaGerenciamentoTarefasApplication.class, args);
+	}
+
+}
